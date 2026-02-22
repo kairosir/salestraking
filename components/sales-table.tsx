c586f3c5082f32d5e19f52ea14753c30b00d5eac
kairosir <44501534+kairosir@users.noreply.github.com>
2026-02-23 00:51:53 +0500

    ui: move floating add/archive/trash buttons to left side

--- components/sales-table.tsx
+++ components/sales-table.tsx
@@ -842,7 +842,7 @@ export function SalesTable({ sales }: { sales: Sale[] }) {
         </div>
       )}
 
-      <div className="fixed bottom-24 right-5 z-40 flex flex-col gap-2 sm:bottom-5">
+      <div className="fixed bottom-24 left-5 z-40 flex flex-col gap-2 sm:bottom-5">
         <button
           type="button"
           onClick={() => setArchiveOpen(true)}
